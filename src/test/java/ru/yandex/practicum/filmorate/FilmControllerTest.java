e6e8f2c5a09ad972c3e0cbc66093cabd74d8269f
Artem <fenris06@rambler.ru>
2023-02-10 18:52:52 +0100

    test: добавлены тесты UserController в клас FilmControllerTest

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -12,7 +12,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
+import ru.yandex.practicum.filmorate.exeption.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
 
 import java.time.LocalDate;
@@ -22,6 +24,7 @@ import java.util.Arrays;
 
 import static java.time.Month.OCTOBER;
 
+import static java.util.Calendar.SEPTEMBER;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,7 +37,8 @@ public class FilmControllerTest {
     private ObjectMapper objectMapper;
 
 
-    @Test
+    @Test /*В этом тесте пришлось тестировать все эндпоинты вмести. Если эндпоинты тестировать по отдельности,
+           то айдишнк фильма будет произвольным образом менятся и тесты перестанут запускаться все вмести*/
     public void shouldValidationPostFilmIfAllFieldsAreCorrectPutAndGet() throws Exception {
         Film film = Film.builder()
                 .id(1)
@@ -68,13 +72,29 @@ public class FilmControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(film1)));
 
-
         mockMvc.perform(
                         MockMvcRequestBuilders.get("/films"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(Arrays.asList(film1))));
     }
 
+    @Test /*Все тесты которые, тестируют аннотации спринга работают корректно*/
+    public void shouldNotValidationPostFilmIfFilmNameIsEmpty() throws Exception {
+        Film film = Film.builder()
+                .id(1)
+                .name("")
+                .description("Фильм про робота убийцу")
+                .releaseDate(LocalDate.of(1984, OCTOBER, 26))
+                .duration(107)
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/films")
+                                .content(objectMapper.writeValueAsString(film))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
 
     @Test
     public void shouldNotValidationAddFilmIfDescriptionNotCorrect() throws Exception {
@@ -98,11 +118,47 @@ public class FilmControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isBadRequest());
-
     }
 
     @Test
-    public void shouldNotValidationAddFilmIfReleaseDateIsBeforeValidDate() throws Exception {
+    public void shouldNotValidationPostFilmIfFilmReleaseDateIsNull() throws Exception {
+        Film film = Film.builder()
+                .id(1)
+                .name("Artem")
+                .description("Фильм про робота убийцу")
+                .releaseDate(null)
+                .duration(107)
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/films")
+                                .content(objectMapper.writeValueAsString(film))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldNotValidationPostFilmIfFilmDurationIsNegative() throws Exception {
+        Film film = Film.builder()
+                .id(1)
+                .name("Artem")
+                .description("Фильм про робота убийцу")
+                .releaseDate(LocalDate.of(1984, OCTOBER, 26))
+                .duration(-1)
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/films")
+                                .content(objectMapper.writeValueAsString(film))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test /*Здесь исключение кастомной валидация тестируется корректно. В тесте аналогичном тесте для юзера
+           исключение не отлавливается*/
+    public void shouldNotCustomValidationAddFilmIfReleaseDateIsBeforeValidDate() throws Exception {
         Film film = Film.builder()
                 .id(1)
                 .name("Artem")
@@ -119,29 +175,207 @@ public class FilmControllerTest {
             );
         });
 
-        assertEquals("Request processing failed;" +
-                " nested exception is ru.yandex.practicum." +
-                "filmorate.exeption.ValidationException:" +
-                " Film release date is before 28.12.1895", thrown.getMessage());
+        assertEquals(thrown.getCause().getClass(), ValidationException.class);
     }
 
-    @Test
-    public void shouldValidationPostFilmIfUserNameIsEmpty() throws Exception {
-        Film film = Film.builder()
+    @Test /*Здесь аналогично тестам фильмов. Все тестирование с правильными полями пришлось засунуть в один тест.
+    Плюс отдельно не получилось создать класс для тестирования эдпоинтов юзера*/
+    public void shouldValidationPostUserIfAllFieldsAreCorrectPutAndGetAndSetNameIsLoginEmpty() throws Exception {
+        User user = User.builder()
                 .id(1)
-                .name("")
-                .description("Фильм про робота убийцу")
-                .releaseDate(LocalDate.of(1984, OCTOBER, 26))
-                .duration(107)
+                .email("Artem@rambler.ru")
+                .login("Fenris")
+                .name("Artem")
+                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
                 .build();
 
         mockMvc.perform(
-                        MockMvcRequestBuilders.post("/films")
-                                .content(objectMapper.writeValueAsString(film))
+                        MockMvcRequestBuilders.post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(user)));
+
+        User user1 = User.builder()
+                .id(1)
+                .email("Artem@rambler.ru")
+                .login("Artem")
+                .name("")
+                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
+                .build();
+
+        User user2 = User.builder()
+                .id(1)
+                .email("Artem@rambler.ru")
+                .login("Artem")
+                .name("Artem")
+                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.put("/users")
+                                .content(objectMapper.writeValueAsString(user1))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(user2)));
+
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(Arrays.asList(user2))));
+    }
+
+    @Test /*Все тесты которые, тестируют аннотации спринга работают корректно*/
+    public void shouldNotValidationPostUserIfEmailIsBlank() throws Exception {
+        User user = User.builder()
+                .id(1)
+                .email("")
+                .login("Fenris")
+                .name("Artem")
+                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/users")
+                                .content(objectMapper.writeValueAsString(user))
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void shouldNotValidationAddUserIfUserEmailDogCharIsFirst() throws Exception {
+        User user = User.builder()
+                .id(1)
+                .email("@Artemrambler.ru")
+                .login("Fenris")
+                .name("Artem")
+                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldNotValidationAddUserIfUserEmailDogCharIsLast() throws Exception{
+        User user = User.builder()
+                .id(1)
+                .email("Artemrambler.ru@")
+                .login("Fenris")
+                .name("Artem")
+                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldNotValidationAddUserIfUserEmailDogCharWithTwoDot() throws Exception{
+        User user = User.builder()
+                .id(1)
+                .email("Artem@rambler..ru")
+                .login("Fenris")
+                .name("Artem")
+                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldNotValidationAddUserIfUserEmailHasSpaseInMiddle() throws Exception{
+        User user = User.builder()
+                .id(1)
+                .email("Arte m@rambler..ru")
+                .login("Fenris")
+                .name("Artem")
+                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldNotValidationAddUserIfUserLoginIsEmpty() throws Exception {
+        User user = User.builder()
+                .id(1)
+                .email("Artem@rambler.ru")
+                .login("")
+                .name("Artem")
+                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldNotValidationAddUserIfUserBirthdayBeInFuture() throws Exception {
+        User user = User.builder()
+                .id(1)
+                .email("Artem@rambler.ru")
+                .login("Fenris")
+                .name("Artem")
+                .birthday(LocalDate.of(2030, SEPTEMBER, 25))
+                .build();
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+    /*Этот тест сделан по аналогии с тестом для фильма.Его запуск приводит к ошибки*/
+//    @Test
+//    public void shouldNotCustomValidationAddUserIfUserLoginHasSpaceAndEmpty() throws Exception{
+//        User user = User.builder()
+//                .id(1)
+//                .email("Artem@rambler.ru")
+//                .login("Y ")
+//                .name("Artem")
+//                .birthday(LocalDate.of(1988, SEPTEMBER, 25))
+//                .build();
+//
+//
+//
+//        Exception thrown = assertThrows(Exception.class, () -> {
+//            mockMvc.perform(
+//                    MockMvcRequestBuilders.post("/users")
+//                            .content(objectMapper.writeValueAsString(user))
+//                            .contentType(MediaType.APPLICATION_JSON)
+//            );
+//        });
+//        assertEquals(thrown.getCause().getClass(), ValidationException.class);
+//
+//    }
 }
 
 
